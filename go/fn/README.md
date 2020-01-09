6ef459f7480534d6761597d512fdfddde078905a
pallat <yod.pallat@gmail.com>
2020-01-09 21:14:53 +0700

    simple main echo

---
+++ go/fn/README.md
@@ -0,0 +1,8 @@
+# Feature
+
+1. logging: logrus
+2. configuration: logrus
+3. client
+4. db
+5. request Id
+6. context
---
+++ go/fn/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+var router = echo.New()
+
+func init() {
+	router.Logger.SetLevel(log.INFO)
+}
+
+func main() {
+	router.GET("/", func(c echo.Context) error {
+		time.Sleep(5 * time.Second)
+		return c.JSON(http.StatusOK, "OK")
+	})
+
+	go start()
+
+	shutdown()
+}
+
+func start() {
+	if err := router.Start(":1323"); err != nil {
+		router.Logger.Info("shutting down the server")
+	}
+}
+
+func shutdown() {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	if err := router.Shutdown(context.Background()); err != nil {
+		router.Logger.Fatal(err)
+	}
+}
